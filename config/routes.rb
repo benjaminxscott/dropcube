e52c71af716d582fdadeaecd5334e493c6243118
Ben <bschmoker+c9@gmail.com>
2018-12-17 05:45:23 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,8 @@ Rails.application.routes.draw do
   get 'upload_files/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+Dropcube::Application.routes.draw do
+   resources :upload_files, only: [:index, :new, :create, :destroy]
+   root "upload_files#index"
+end
