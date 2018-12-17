95b82034394d9b75b081058583b7f5e41010d134
Ben <bschmoker+c9@gmail.com>
2018-12-17 05:48:00 +0000

    add controller

--- app/controllers/upload_files_controller.rb
+++ app/controllers/upload_files_controller.rb
@@ -1,13 +1,32 @@
 class UploadFilesController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
-  def create
-  end
-
-  def destroy
-  end
+   def index
+      @upload_files = UploadFile.all
+   end
+   
+   def new
+      @upload_file = UploadFile.new
+   end
+   
+   def create
+      @upload_file = UploadFile.new(upload_file_params)
+      
+      if @upload_file.save
+         redirect_to upload_files_path, notice: "The upload_file #{@upload_file.name} has been uploaded."
+      else
+         render "new"
+      end
+      
+   end
+   
+   def destroy
+      @upload_file = UploadFile.find(params[:id])
+      @upload_file.destroy
+      redirect_to upload_files_path, notice:  "The upload_file #{@upload_file.name} has been deleted."
+   end
+   
+   private
+      def upload_file_params
+      params.require(:upload_file).permit(:name, :attachment)
+   end
+   
 end
